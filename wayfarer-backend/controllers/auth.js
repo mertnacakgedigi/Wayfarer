10c929abf0d782f5e35b19c986f210915775e06c
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-04-23 10:07:34 -0700

    auth controller done and routes created

--- wayfarer-backend/controllers/auth.js
+++ wayfarer-backend/controllers/auth.js
@@ -52,3 +52,54 @@ const register = (req, res) => {
     })
 }
 
+
+const login = (req ,res ) => {
+    if (!req.body.username || ! req.body.passsword) {
+        return res.status(400).json({status : 400, message : 'Please enter your username and password'})
+    }
+
+    db.User.findOne({username:req.body.username}, (err,foundUser) => {
+        if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again' });
+
+        if(!foundUser) {
+            if (err) return res.status(500).json({ status: 500, message: 'Username or password is incorret' });
+        }
+
+        bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+            if (err) return res.status(500).json({ status: 500, message: 'Try with another password' });
+
+            if(isMatch) {
+                req.session.currentUser = {id : foundUser._id}
+                return res.status(200).json({status: 200, message : "Succes", data : foundUser._id})
+            } else {
+                return res.status(400).json({ status: 400, message: 'Username or password is incorret' });
+            }
+        })
+    })
+}                                          
+
+const logout = (req, res) => {
+    if(!req.session.currentUser) return res.status(401).json({status :401, message : 'Unauthorized'})
+
+    req.session.destroy((err)=> {
+        if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again' });
+        res.sendStatus(200)
+    })
+}
+
+
+const verify = (req, res) => {
+    if(!req.session.currentUser) return res.status(401).json({status : 401, message: 'Unauthorized'})
+    res.status(200).json({
+        status : 200,
+        message : `Current user verified. User ID : ${req.session.currentUser.id}`,
+        currentUser : req.session.currentUser
+    })
+}
+
+module.exports  = {
+    register,
+    login,
+    verify,
+    logout
+}

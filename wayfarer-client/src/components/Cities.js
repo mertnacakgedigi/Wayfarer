42afaddbe4545e2dd4cd806743a05c06953f3c54
huangca <hch0612@gmail.com>
2020-04-23 21:44:22 -0700

    profile update

--- wayfarer-client/src/components/Cities.js
+++ wayfarer-client/src/components/Cities.js
@@ -8,10 +8,38 @@ export default class Cities extends React.Component {
 	}
 
 	render() {
+		const cities=[
+			{ _id:1, name: 'LA',image: "#",description : "test description",posts : [],},
+			{ _id:2, name: 'SF',image: "#",description : "test description",posts : [],},
+			{ _id:2, name: 'LA',image: "#",description : "test description",posts : [],},
+
+		]
+
+
+
 		return (
-			<div className='container'>
-				<h1>cities</h1>
-			</div>
+			<>
+			{cities.map((city)=>(
+				
+					<div class="container">
+					  <div class="row gamerow" id="#">
+					    <div class="col-sm">
+					      <img src={city.image} class="img-thumbnail" alt={city.name} />
+					    </div>
+					    <div class="col-sm">
+					     <h2> <a className="nav-link" href="/city/">{city.name}</a></h2>
+					     
+					     <h5>descroption: {city.description} </h5>
+					    </div>
+					    <div class="col-sm">
+					      Has {city.posts.length} {city.posts.length === 1 ? ' review' : ' reviews'} 
+					    </div>
+					  </div>
+					</div>
+				
+
+				))}
+			</>
 		);
 	}
 }
---
+++ wayfarer-client/src/components/city.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class city extends React.Component {
+
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+			<h1>city</h1>
+			</div>
+		);
+	}
+}
